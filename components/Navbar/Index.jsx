3fa3ed629714e6fd1525491181bb376f99c0198c
qalbeali <qalbeali8@gmail.com>
2022-09-22 07:56:48 -0700

    updated the signup form

--- components/Navbar/Index.jsx
+++ components/Navbar/Index.jsx
@@ -300,25 +300,29 @@ const Navbar = () => {
 
                                                         <div className="flex  py-6 ">
                                                             <div className="w-full px-3">
-                                                                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+                                                                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"  >
                                                                     Phone No
                                                                 </label>
-                                                                <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="number" placeholder="Phone No" />
+                                                                <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" placeholder="Phone No" />
                                                             </div>
-                                                            <div className="w-full px-3">
-                                                                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+                                                            <div className="w-full md:w-1/2.5 px-3 mb-6 md:mb-0">
+                                                                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
                                                                     Role
                                                                 </label>
-                                                                <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="string" placeholder="Admin" />
+                                                                <div className="relative">
+                                                                    <select className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
+                                                                        <option>Owner</option>
+                                                                        <option>Agent</option>
+
+                                                                    </select>
+                                                                    <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                                                                        <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
+                                                                    </div>
+                                                                </div>
                                                             </div>
                                                         </div>
                                                     </div>
-                                                    <div className="w-full ">
-                                                        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
-                                                            UserName
-                                                        </label>
-                                                        <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="string" placeholder="Admin" />
-                                                    </div>
+
                                                     <div className="w-full ">
                                                         <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
                                                             Email
